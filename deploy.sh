fa6e3a94e014ad478f07b95bc844281b9e2971fc
Ravish <211128@iiitt.ac.in>
2023-03-16 22:20:56 +0530

    add version 1.1

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# echo "# tindog-fully-responsive" >> README.md
+# git init
+# git add README.md
+git add .
+git commit -m "add version 1.1"
+git branch -M main
+git remote add origin https://github.com/Ravish108Coder/tindog-fully-responsive.git
+git push -u origin main
